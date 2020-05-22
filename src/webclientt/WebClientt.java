c3df1d39fb096a976360f834ec8950a91518a9d7
nafe hamad <nafehamad90@gmail.com>
2020-05-23 00:44:26 +0300

    This is a web programming project/ http protocol

---
+++ src/webclientt/WebClientt.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package webclientt;
+
+/**
+ *
+ * @author hp
+ */
+public class WebClientt {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      frame f= new frame();
+      f.setVisible(true);// TODO code application logic here
+    }
+    
+}
